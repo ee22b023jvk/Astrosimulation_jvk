247f6ffcc4ec98f4f87d03422fa05cabae52ebe4
ee22b023jvk <133376250+ee22b023jvk@users.noreply.github.com>
2023-05-14 22:48:37 +0530

    Add files via upload

---
+++ celestialbodies.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+double G = 6.6743 * pow(10,-11);
+
+
+using namespace std;
+
+class celestialBody{
+    public:
+    double mass, radius, density, spin, distance_from_earth, surface_temp;
+    string major_composition;
+    
+    double surfaceGravity(){
+        return G*mass/radius*radius;
+    }
+
+};
+
+class star : celestialBody{
+    public:
+    double Luminosity, wavelength;
+    double intensityOfLightOnEarth(){
+        return Luminosity/4*M_PI*pow(distance_from_earth, 2);
+    }
+};
+
+class planet : celestialBody{
+    public:
+    int number_of_moons, number_of_rings;
+    double orbial_speed;
+    bool habitability;
+};
